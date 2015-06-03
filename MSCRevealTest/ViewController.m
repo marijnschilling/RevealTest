90404bb3c857c853341e5c1d12282748cd792ebf
Marijn Schilling <marijn@fonk-amsterdam.com>
2015-06-03 13:33:28 +0200

    Create project

---
+++ MSCRevealTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MSCRevealTest
+//
+//  Created by Marijn Schilling on 03/06/15.
+//  Copyright (c) 2015 marijn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
